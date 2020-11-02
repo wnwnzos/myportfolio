c35c83d20e777a8e782116390ef5e9b12f47bd83
wnwnzos <69783491+wnwnzos@users.noreply.github.com>
2020-11-02 16:01:22 +0900

    z

--- js/main.js
+++ js/main.js
@@ -11,10 +11,7 @@ $(function () { // jQB ///////////////////////////
         navigationTooltips: ['Page1', 'Page2', 'Page3', 'Page4', 'Page5'],
     });
 
-    //
-    //    $(".scrollIcon span").clcik(function () {
-    //        location = "#sec2";
-    //    });
+   
 
 
 
@@ -37,26 +34,17 @@ $(function () { // jQB ///////////////////////////
 
 
     new Swiper('.swiper-container', {
-
-
-        slidesPerView: 1, // 동시에 보여줄 슬라이드 갯수
-
-        slidesPerGroup: 1, // 그룹으로 묶을 수, slidesPerView 와 같은 값을 지정하는게 좋음
-
-        // 그룹수가 맞지 않을 경우 빈칸으로 메우기
-        // 3개가 나와야 되는데 1개만 있다면 2개는 빈칸으로 채워서 3개를 만듬
-        loopFillGroupWithBlank: true,
-
-        //블릿
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
         pagination: {
             el: '.swiper-pagination',
-            type: 'bullets',
+
         },
 
 
 
     }); /*swiper플러그인*/
-
 
 
 
