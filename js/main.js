e836d345d0e9ace9b2cb2e76ed970d9fbeb6c19c
wnwnzos <69783491+wnwnzos@users.noreply.github.com>
2020-12-11 18:58:43 +0900

    sd

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function () { // jQB ///////////////////////////
 
-    
+
 
     // 풀페이지 플러그인 호출
     $("#fullpage").fullpage({
@@ -11,47 +11,56 @@ $(function () { // jQB ///////////////////////////
         navigationPosition: 'right',
         /*네비블릿 오버시 나타나는 글씨*/
         navigationTooltips: ['home', 'about', 'skill', 'portfolio', 'contact'],
-        /*스킬페이지 도착했을때 그래프채우기*/
+
         'afterLoad': function (anchorLink, index) {
             if (index == 2) {
-                $('.about_wrap').fadeIn(500);
-            }
-            else if (index == 3) {//스킬부분
+                $('.about_wrap').fadeIn(300);
+            } else if (index == 3) { //스킬부분 그래프채우기
                 $(".sec3").animate({
-                    opacity:"1"
-                },500);
+                    opacity: "1"
+                }, 500);
                 $('.graph span')
                     .addClass('active');
-            }
-            else if (index == 4) {//포트폴리오부분
+            } else if (index == 4) { //포트폴리오부분
                 $(".port_wrap").animate({
-                  opacity:"1"   
-                },500);
-            }
-            else if (index == 5) {//컨텍트부분
+                    opacity: "1"
+                }, 500);
+            } else if (index == 5) { //컨텍트부분
                 $(".cont_wrap").animate({
-                    opacity:"1"
-                },500);
+                    opacity: "1"
+                }, 500);
             }
-         
+
         },
 
+
+
+
+    });
+    // 모달창 스크롤막기
+    $(document).on('click', '.show', function () {
+        $.fn.fullpage.setAllowScrolling(false);
+        $.fn.fullpage.setKeyboardScrolling(false);
     });
 
-   
+    $(document).on('click', '.cbtn', function () {
+        $.fn.fullpage.setAllowScrolling(true);
+        $.fn.fullpage.setKeyboardScrolling(true);
+    });
 
-    
+
+
     /*about영역*****************************/
     var aboutbtn = $(".licencse").find(".detail_btn")
-    
-    aboutbtn.click(function(e){
-       e.preventDefault();
+
+    aboutbtn.click(function (e) {
+        e.preventDefault();
         $(".licencse").hide();
         $(".lishow").stop().fadeIn(1000);
     });
-    
-    
-     
+
+
+
 
 
 
@@ -68,22 +77,23 @@ $(function () { // jQB ///////////////////////////
         smartBackspace: false,
         loop: true
     });
-    
+
     /*mbti클릭시 보이기*/
-    $(".show").click(function(){
-        $(this).next(".bottom").animate({
-            left:"0"
-        },500)
-    });// click /////////////////////
-    
-    $(".cbtn").click(function(){
-        $(this).parent(".bottom").animate({
-            left:"200%"
-        },500)
-    });// click /////////////////////
-    
-    
-    
+    $(".modal_overlay").hide();
+    $(".show").click(function () {
+
+
+        $(".modal_overlay").fadeIn(500);
+    });
+
+    $(".cbtn").click(function () {
+        $(".modal_overlay").fadeOut(500);
+    });
+
+
+
+
+
 
 
     /*포트폴리오 스와이퍼 영역************************************/
@@ -95,7 +105,7 @@ $(function () { // jQB ///////////////////////////
         loop: true,
         pagination: {
             el: '.swiper-pagination',
-            clickable:true,
+            clickable: true,
 
         },
 
@@ -113,6 +123,3 @@ $(function () { // jQB ///////////////////////////
 }); //////////////// jQB ////////////////////////
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
-
-
-
