0f5eac06bc2ff83d1fe87e1807b531b35a83db5e
wnwnzos <69783491+wnwnzos@users.noreply.github.com>
2020-12-15 22:47:40 +0900

    s

--- js/main.js
+++ js/main.js
@@ -13,22 +13,71 @@ $(function () { // jQB ///////////////////////////
         navigationTooltips: ['home', 'about', 'skill', 'portfolio', 'contact'],
 
         'afterLoad': function (anchorLink, index) {
-            if (index == 2) {
-                $('.about_wrap').fadeIn(300);
+            if (index == 1) {// 홈영역
+
+                $(".toptit").animate({
+                    top: "1.9%"
+                }, 800);
+                $(".bottomtit").delay(500).animate({
+                    bottom: "-2.5%"
+                }, 700);
+                $("nav>ul").delay(1000).animate({
+                    bottom: "5%"
+                }, 600);
+            } else if (index == 2) {// 어바웃영역
+                $(".about_tit").animate({
+                    right: "0"
+                }, 500);
+                $(".topshow").delay(500).animate({
+                    opacity: 1
+                }, 800);
+                $(".atit small").delay(1000).animate({
+                    opacity: 1
+                }, 800);
+                $(".mypic").delay(1500).animate({
+                    opacity: 1
+                }, 800);
+                $(".info i").delay(2000).animate({
+                    opacity: 1
+                }, 800);
+                $(".info_tit").delay(2500).animate({
+                    opacity: 1
+                }, 800);
             } else if (index == 3) { //스킬부분 그래프채우기
-                $(".sec3").animate({
+                $(".skill_tit").animate({
+                    right: "0"
+                }, 800);
+                $(".left_wrap").animate({
+                    opacity: "1"
+                }, 500);
+                $(".right_wrap").delay(500).animate({
                     opacity: "1"
                 }, 500);
                 $('.graph span')
                     .addClass('active');
+                $(".right_sub").delay(1000).animate({
+                    opacity: "1"
+                }, 500);
             } else if (index == 4) { //포트폴리오부분
-                $(".port_wrap").animate({
+                $(".portfolio_tit").animate({
+                    right: "0"
+                }, 800);
+                $(".slidewr .left").delay(800).animate({
                     opacity: "1"
-                }, 500);
+                }, 800);
+                $(".port_wrap > big").delay(1000).animate({
+                    right: "0"
+                }, 800);
+                $(".detailTxt_box svg").delay(1500).animate({
+                     width: "10vw"
+                }, 800);
             } else if (index == 5) { //컨텍트부분
-                $(".cont_wrap").animate({
-                    opacity: "1"
-                }, 500);
+                $(".contact_tit").animate({
+                    right: "0"
+                }, 800);
+                $(".cont_wrap").delay(600).animate({
+                    left: 0
+                }, 800);
             }
 
         },
@@ -37,29 +86,17 @@ $(function () { // jQB ///////////////////////////
 
 
     });
-    // 모달창 스크롤막기
-    $(document).on('click', '.show', function () {
-        $.fn.fullpage.setAllowScrolling(false);
-        $.fn.fullpage.setKeyboardScrolling(false);
-    });
-
-    $(document).on('click', '.cbtn', function () {
-        $.fn.fullpage.setAllowScrolling(true);
-        $.fn.fullpage.setKeyboardScrolling(true);
-    });
 
 
 
     /*about영역*****************************/
-    var aboutbtn = $(".licencse").find(".detail_btn")
 
-    aboutbtn.click(function (e) {
-        e.preventDefault();
-        $(".licencse").hide();
-        $(".lishow").stop().fadeIn(1000);
+
+    $(".plusbtn").click(function () {
+        $(".infobox").animate({
+            opacity: 1
+        }, 500);
     });
-
-
 
 
 
@@ -69,25 +106,13 @@ $(function () { // jQB ///////////////////////////
     /********** 타이핑 효과 ********/
 
     var typed5 = new Typed('#typed5', {
-        strings: ['안녕하세요.  웹세상에서 소통할수 있는 웹 퍼블리셔, 박혜주입니다.'],
-        typeSpeed: 50,
+        strings: ['이 사이트는 포트폴리오용으로 제작되었습니다.'],
+        typeSpeed: 65,
         backSpeed: 0,
         cursorChar: '_',
         shuffle: true,
         smartBackspace: false,
         loop: true
-    });
-
-    /*mbti클릭시 보이기*/
-    $(".modal_overlay").hide();
-    $(".show").click(function () {
-
-
-        $(".modal_overlay").fadeIn(500);
-    });
-
-    $(".cbtn").click(function () {
-        $(".modal_overlay").fadeOut(500);
     });
 
 
@@ -102,16 +127,40 @@ $(function () { // jQB ///////////////////////////
     new Swiper('.swiper-container', {
         slidesPerView: 1,
         spaceBetween: 40,
-        loop: true,
-        pagination: {
+        loop: true, // 무한반복
+        pagination: { // 페이징
+
             el: '.swiper-pagination',
-            clickable: true,
+
+            clickable: true, // 페이징을 클릭하면 해당 영역으로 이동, 필요시 지정해 줘야 기능 작동
+
+        },
+
+        navigation: { // 네비게이션
+
+            nextEl: '.swiper-button-next', // 다음 버튼 클래스명
+
+            prevEl: '.swiper-button-prev', // 이번 버튼 클래스명
 
         },
 
 
-
     }); /*swiper플러그인*/
+
+
+    // 사이트보기 버튼 마우스 오버/아웃시 class넣기/빼기
+    
+    
+    $(".detail_btn").mouseenter(function () {
+        $(this).addClass("on");
+         $(this).removeClass("off");
+    });
+    
+    $(".detail_btn").mouseleave(function () {
+        console.log("아웃");
+         $(this).removeClass("on");
+         $(this).addClass("off");
+    });
 
 
 
