44b4a8c4df77058a304ec107241d3afcfb5c20b3
wnwnzos <69783491+wnwnzos@users.noreply.github.com>
2020-12-28 18:14:50 +0900

    z

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 $(function () { // jQB ///////////////////////////
 
-if (navigator.userAgent.indexOf('iPhone')!=-1)
-  {
-   addEventListener("load",function()
-    {
-    setTimeout(hideURLbar,0);
-    },false);
-  }
-// 위에껀 아이폰
-// 요 아래껀 다른폰
-  function hideURLbar()
-  {
-    window.scrollTo(0,1);
-  }
 
-    
+    function hideAddressBar() {
+
+        if (document.height <= window.outerHeight) return; // 웹페이지의 높이가 화면높이보다 작을 때는 실행할 필요가 없으므로 종료
+
+        var scrollTimer = setInterval(function () {
+            if (!pageYOffset) { // 모바일 브라우저에서 주소창이 보이고 있을 때는 pageYOffset = 0 이므로 이때만 실행
+                scrollTo(0, 1); // 웹페이지를 x축 0, y축 1의 위치로 끌어올림
+            } else {
+                clearInterval(scrollTimer); // pageYOffset !=0 인 경우 반복 종료: scrollTo(0, 1) 이 실행되었거나 사용자가 스크롤을 움직인 경우
+            }
+        }, 100); // 100 밀리세컨드마다 반복 실행
+
+    };
+
+    window.addEventListener('load', hideAddressBar, false); // 페이지 로드 되었을 때 실행
+    window.addEventListener('orientationchange', hideAddressBar, false);
+
     
     
     // 풀페이지 플러그인 호출
