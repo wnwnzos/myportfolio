21caca0cd7a2c79f12dd50a071d795b21fe5444e
wnwnzos <69783491+wnwnzos@users.noreply.github.com>
2020-12-28 18:19:50 +0900

    z

--- js/main.js
+++ js/main.js
@@ -1,25 +1,31 @@
 $(function () { // jQB ///////////////////////////
 
 
-    function hideAddressBar() {
+    window.addEventListener('load', function () {
 
-        if (document.height <= window.outerHeight) return; // 웹페이지의 높이가 화면높이보다 작을 때는 실행할 필요가 없으므로 종료
+        setTimeout(scrollTo, 0, 0, 1)
 
-        var scrollTimer = setInterval(function () {
-            if (!pageYOffset) { // 모바일 브라우저에서 주소창이 보이고 있을 때는 pageYOffset = 0 이므로 이때만 실행
-                scrollTo(0, 1); // 웹페이지를 x축 0, y축 1의 위치로 끌어올림
-            } else {
-                clearInterval(scrollTimer); // pageYOffset !=0 인 경우 반복 종료: scrollTo(0, 1) 이 실행되었거나 사용자가 스크롤을 움직인 경우
-            }
-        }, 100); // 100 밀리세컨드마다 반복 실행
+    }, false);
 
-    };
 
-    window.addEventListener('load', hideAddressBar, false); // 페이지 로드 되었을 때 실행
-    window.addEventListener('orientationchange', hideAddressBar, false);
+    addEventListener("load", function () {
+        setTimeout(updateLayout, 0);
+    }, false);
+    var currentWidth = 0;
 
-    
-    
+    function updateLayout() {
+        if (window.innerWidth != currentWidth) {
+            currentWidth = window.innerWidth;
+            var orient = currentWidth == 320 ? "profile" : "landscape";
+            document.body.setAttribute("orient", orient);
+            setTimeout(function () {
+                window.scrollTo(0, 1);
+            }, 100);
+        }
+    }
+    setInterval(updateLayout, 400);
+
+
     // 풀페이지 플러그인 호출
     $("#fullpage").fullpage({
 
@@ -351,7 +357,7 @@ $(function () { // jQB ///////////////////////////
 
 
 
-    
+
 
 
 
